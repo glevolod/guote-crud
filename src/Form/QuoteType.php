a1bc177e6577ff6198f985d22f24839666b8cf05
Glechikov Vladimir <glevolod@gmail.com>
2020-07-27 13:57:55 +0300

    Create basic CRUD

---
+++ src/Form/QuoteType.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Author;
+use App\Entity\Quote;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class QuoteType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('text')
+            ->add('author', EntityType::class, [
+                'class' => Author::class,
+                'choice_label' => 'name'
+            ])
+            ->add('type', EntityType::class, [
+                'class' => \App\Entity\QuoteType::class,
+                'choice_label' => 'name'
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Quote::class,
+        ]);
+    }
+}
